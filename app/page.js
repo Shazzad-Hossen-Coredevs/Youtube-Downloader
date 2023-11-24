b87914a625a9c6853d5746f0743461db6dbb6d0e
Shazzad-Hossen-Coredevs <shazzad_hossen@coredevs.ltd>
2023-11-24 18:19:02 +0600

    yt video downloader using puppeteer

--- app/page.js
+++ app/page.js
@@ -1,34 +1,49 @@
-"use client"
-import Image from 'next/image';
-import React, { useState } from 'react';
-import logo from './assets/logo.png';
-import yeii from './assets/excited.png';
+import React from 'react';
+import Ui from './components/UI';
+import puppeteer from 'puppeteer';
+import { toast } from 'react-toastify';
 
-const page = () => {
-  const [inputVal, setInputVal]= useState('');
-  const handleDownload = () => {
-    console.log(inputVal);
+const Home = async ({ searchParams }) => {
+  let data = null;
+  if (searchParams.url) {
+    data = await handleDownload(searchParams.url);
+    // console.log(data);
+
   }
   return (
     <div>
-      <div className='bg-sky-100 border-b border-sky-200'>
-        <div className='p-5 max-w-[1200px] w-full mx-auto text-white flex justify-between items-center'>
-          <div className='flex items-center gap-3 text-sky-800 text-3xl font-[600]'><Image src={logo} className='w-[50px]' alt='logo' /> YT DOWNLOADER</div>
-          <div>
-            {/* Other Nav Options */}
-          </div>
-        </div>
-      </div>
-      <main className='max-w-[1200px] w-full mx-auto'>
-
-        <div className='max-w-[500px] w-full mx-auto mt-20'>
-          <input type='text' className='border border-sky-300 rounded-lg p-6 w-full outline-sky-600 text-lg text-sky-800' placeholder='Youtube video url' value={inputVal} onChange={(e)=> setInputVal(e.target.value)}/>
-          <div className='flex items-center gap-2 justify-center p-5 rounded-lg bg-sky-300 mt-5 text-sky-800 font-[600] drop-shadow text-xl cursor-pointer active:scale-95 select-none shadow-md' onClick={handleDownload}><Image src={yeii} alt='Yeii Icon' className='w-[30px]'/> Yeii. I am feeling lucky</div>
-        </div>
-
-      </main>
+      <Ui data={data} />
     </div>
   );
 };
 
-export default page;
+export default Home;
+
+
+const handleDownload = async (url) => {
+ try {
+  
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://yt1s.ltd/en182hzb/');
+  await page.waitForTimeout(1000);
+  page.type('#txt-url', url);
+  await page.waitForTimeout(1000);
+  page.click('#btn-submit');
+  await page.waitForTimeout(4000)
+  const element = await page.waitForXPath('/html/body/div[1]/div[1]/div[5]/div/div[1]/div/div[1]/table/tbody/tr[1]/td[3]/a');
+  const downloadUrl = await page.evaluate(anchor => anchor.getAttribute('href'), element);
+  const thumbnail = await page.waitForSelector('.img-thumbnail');
+  const thumbUrl = await page.evaluate(anchor => anchor.getAttribute('src'), thumbnail);
+  const videoTitle = await page.$eval(`#video_title`, element => element.innerText);
+  
+
+  await browser.close();
+
+  return ({ url: downloadUrl, thumbUrl: thumbUrl, title: videoTitle })
+ } catch (error) {
+  return(null);
+  
+ }
+
+}
